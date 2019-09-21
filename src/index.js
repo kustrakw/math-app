cc9574b7445219a4acee7d5d45ad3b0d55918c39
kustrakw <kustrakw@nextgen.com>
2019-09-21 07:25:33 -0700

    Lifted state from Problem to Game; Added Problem refresh; Updated Problem formatting

--- src/index.js
+++ src/index.js
@@ -2,85 +2,106 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Problem extends React.Component {
+
+function Problem(props) {
+    let num1Margin = '33px';
+    let num2Margin = '10px'
+
+    if (props.num1 > 9 || props.num2 > 9) {
+        if (props.num1 < 10) {
+            num1Margin = '45px';
+        }
+        if(props.num2 < 10) {
+            num2Margin = '15px';
+            num1Margin = '24px'
+        }        
+    }
+
+    return(
+    <div>
+        <p className='number' style={{marginLeft: num1Margin}}>{props.num1}</p>
+        <p className='operator' style={{marginLeft: num2Margin}}>{props.operator} {props.num2}</p>
+        <p className='equals'>____</p>
+    </div>
+    )
+} 
+
+class Game extends React.Component {
     constructor(props) {
         super(props);
-        const min = 1;
-        const max = 10;
-        const rand1 = min + Math.random() * (max + 1 - min);
-        const rand2 = min + Math.random() * (max + 1 - min);
+        
         this.state = {
-            'answer': 0,
-            'num1': parseInt(rand1,10),
-            'num2': parseInt(rand2, 10),
-            'outcome': null,
+            num1: this.generateRandmoNumber(),
+            num2: this.generateRandmoNumber(),
+            outcome: null,
+            input: '',
         }
 
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSelect = this.handleSelect.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        if (this.calculateAnswer() === parseInt(this.state.input,10)){
+            this.setState({outcome: 'Correct!!'});
+            setTimeout(() => {
+                    this.setState({
+                    num1: this.generateRandmoNumber(),
+                    num2: this.generateRandmoNumber(),
+                    outcome: null,
+                    input: '',
+                });
+            }, 1000);
+        }
+        else {
+            this.setState({outcome: 'Incorrect.  Please try again.'})
+        }
+        event.preventDefault();
     }
 
     handleChange(event) {
         this.setState({
-            answer: event.target.value,
+            input: event.target.value,
         });
-      }
-    
+    }
+
     handleSelect(event) {
         this.setState({
             outcome: null
         });
     }
 
-    handleSubmit(event) {
-        if (parseInt(this.state.answer, 10) === this.calculateAnswer()){
-            this.setState({outcome: 'Correct!!'})
-        }
-        else {
-            this.setState({outcome: 'Incorrect.  Please try again.'})
-        }
-        event.preventDefault();
-      }
-    
+    generateRandmoNumber() {
+        const min = 1;
+        const max = 10;
+        return parseInt(min + Math.random() * (max + 1 - min), 10);
+    }
+
     calculateAnswer() {
         return this.state.num1 + this.state.num2;
     }
     
     render() {
-        let status;
-        if(this.state.outcome !=null) {
-            status = this.state.outcome
-        }
-        else {
-            status = ''
-        }
         return(
             <form onSubmit={this.handleSubmit}>
-                <p className='number'>{this.state.num1}</p>
-                <p className='operator'>+ {this.state.num2}</p>
-                <p className='equals'>____</p>
+                <Problem
+                num1 = {this.state.num1}
+                num2 = {this.state.num2}
+                operator = '+'
+                />
                 <label className='input'>
                     <input
                         type='text'
+                        value={this.state.input}
                         style={{fontSize:'22px', width:'45px'}}
-                        autoFocus='true'
+                        autoFocus={true}
                         onChange={this.handleChange}
                         onSelect={this.handleSelect}
                     />
                 </label>
-                {/* <input type="submit" value="Submit" /> */}
-                <p>{status}</p>
+                <p>{this.state.outcome}</p>
             </form>
-        ) 
-    }
-}
-
-class Game extends React.Component {
-
-    render() {
-        return(
-            <Problem />
         )
     }
 }
