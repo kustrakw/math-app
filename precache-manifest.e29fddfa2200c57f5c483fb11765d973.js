a714ba57719f7f6a17cf99571c03ead9da7e1785
kustrakw <kustrakw@nextgen.com>
2019-09-23 15:12:37 -0700

    Updates

---
+++ precache-manifest.e29fddfa2200c57f5c483fb11765d973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d17ced7a7fcb1268cdde4fa2585128",
+    "url": "/math-app/index.html"
+  },
+  {
+    "revision": "c9c75ec4f609576c8820",
+    "url": "/math-app/static/css/2.1ad9c816.chunk.css"
+  },
+  {
+    "revision": "339464b4c457bd9e6299",
+    "url": "/math-app/static/css/main.28ef34a0.chunk.css"
+  },
+  {
+    "revision": "c9c75ec4f609576c8820",
+    "url": "/math-app/static/js/2.b3056512.chunk.js"
+  },
+  {
+    "revision": "339464b4c457bd9e6299",
+    "url": "/math-app/static/js/main.f3b47491.chunk.js"
+  },
+  {
+    "revision": "aa3e0cdde931d25c3817",
+    "url": "/math-app/static/js/runtime~main.6bd5bbbe.js"
+  }
+]);
